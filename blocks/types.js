1b91cd8df3876ae54c27b763b7131d9d35957cfb
rink1969 <rink1969@cryptape.com>
2019-10-31 15:41:28 +0800

    add ckb types

--- blocks/types.js
+++ blocks/types.js
@@ -4,7 +4,7 @@ goog.provide('Blockly.CKB.types');
 
 Blockly.defineBlocksWithJsonArray([
   {
-    "type": "outpoint",
+    "type": "ckb_types_outpoint",
     "message0": "%1 %2",
     "args0": [
       {
@@ -20,7 +20,132 @@ Blockly.defineBlocksWithJsonArray([
     ],
     "output": "String",
     "colour": 230,
-    "tooltip": "outpoint",
+    "tooltip": "OutPoint",
+    "helpUrl": ""
+  },
+  {
+    "type": "ckb_types_cell_dep",
+    "message0": "%1 %2",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "out_point",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "dep_type",
+        "check": "String"
+      }
+    ],
+    "output": "String",
+    "colour": 230,
+    "tooltip": "CellDep",
+    "helpUrl": ""
+  },
+  {
+    "type": "ckb_types_input",
+    "message0": "%1 %2",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "previous_output",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "since",
+        "check": "Number"
+      }
+    ],
+    "output": "String",
+    "colour": 230,
+    "tooltip": "Input",
+    "helpUrl": ""
+  },
+  {
+    "type": "ckb_types_script",
+    "message0": "%1 %2 %3",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "code_hash",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "args",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "hash_type",
+        "check": "String"
+      }
+    ],
+    "output": "String",
+    "colour": 195,
+    "tooltip": "Script",
+    "helpUrl": ""
+  },
+  {
+    "type": "ckb_types_output",
+    "message0": "%1 %2 %3",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "capacity",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "lock",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "type",
+        "check": "String"
+      }
+    ],
+    "output": "String",
+    "colour": 195,
+    "tooltip": "Output",
+    "helpUrl": ""
+  },
+  {
+    "type": "ckb_types_transaction",
+    "message0": "%1 %2 %3 %4 %5",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "cell_deps",
+        "check": "Array"
+      },
+      {
+        "type": "input_value",
+        "name": "inputs",
+        "check": "Array"
+      },
+      {
+        "type": "input_value",
+        "name": "outputs",
+        "check": "Array"
+      },
+      {
+        "type": "input_value",
+        "name": "outputs_data",
+        "check": "Array"
+      },
+      {
+        "type": "input_value",
+        "name": "witnesses",
+        "check": "Array"
+      }
+    ],
+    "output": "String",
+    "colour": 195,
+    "tooltip": "Transaction",
     "helpUrl": ""
   }
 ]);
--- generators/javascript/types.js
+++ generators/javascript/types.js
@@ -4,9 +4,54 @@ goog.provide('Blockly.JavaScript.ckb_types');
 
 goog.require('Blockly.JavaScript');
 
-Blockly.JavaScript['outpoint'] = function(block) {
+Blockly.JavaScript['ckb_types_outpoint'] = function(block) {
   var value_tx_hash = Blockly.JavaScript.valueToCode(block, 'tx_hash', Blockly.JavaScript.ORDER_ATOMIC);
   var value_index = Blockly.JavaScript.valueToCode(block, 'index', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = '{' + 'tx_hash:' + value_tx_hash + ',' + 'index:' + value_index + '}';
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  var code = '{' + 'tx_hash:' + value_tx_hash + ',' + 'index:0x' + parseInt(value_index, 10).toString(16) + '}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ckb_types_cell_dep'] = function(block) {
+  var value_out_point = Blockly.JavaScript.valueToCode(block, 'out_point', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_dep_type = Blockly.JavaScript.valueToCode(block, 'dep_type', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = '{' + 'out_point:' + value_out_point + ',' + 'dep_type:' + value_dep_type + '}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ckb_types_input'] = function(block) {
+  var value_previous_output = Blockly.JavaScript.valueToCode(block, 'previous_output', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_since = Blockly.JavaScript.valueToCode(block, 'since', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = '{' + 'previous_output:' + value_previous_output + ',' + 'since:0x' + parseInt(value_since, 10).toString(16) + '}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ckb_types_script'] = function(block) {
+  var value_code_hash = Blockly.JavaScript.valueToCode(block, 'code_hash', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_args = Blockly.JavaScript.valueToCode(block, 'args', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_hash_type = Blockly.JavaScript.valueToCode(block, 'hash_type', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = '{' + 'code_hash:' + value_code_hash + ',' + 'args:' + value_args + ',' + 'hash_type:' + value_hash_type + '}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ckb_types_output'] = function(block) {
+  var value_capacity = Blockly.JavaScript.valueToCode(block, 'capacity', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_lock = Blockly.JavaScript.valueToCode(block, 'lock', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_type = Blockly.JavaScript.valueToCode(block, 'type', Blockly.JavaScript.ORDER_ATOMIC) || 'nil';
+  var code = '{' + 'capacity:0x' + parseInt(value_capacity, 10).toString(16) + ',' + 'lock:' + value_lock + ',' + 'type:' + value_type + '}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ckb_types_transaction'] = function(block) {
+  var value_cell_deps = Blockly.JavaScript.valueToCode(block, 'cell_deps', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_inputs = Blockly.JavaScript.valueToCode(block, 'inputs', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_outputs = Blockly.JavaScript.valueToCode(block, 'outputs', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_outputs_data = Blockly.JavaScript.valueToCode(block, 'outputs_data', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_witnesses = Blockly.JavaScript.valueToCode(block, 'witnesses', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = '{version: 0x0, header_deps: [],'
+           + 'cell_deps:' + value_cell_deps + ','
+           + 'inputs:' + value_inputs + ','
+           + 'outputs:' + value_outputs + ','
+           + 'outputs_data:' + value_outputs_data + ','
+           + 'witnesses:' + value_witnesses + '}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
