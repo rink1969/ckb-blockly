24258ed6fa32bfcdd110d95e22d0dc410673f226
rink1969 <rink1969@cryptape.com>
2019-10-31 16:27:54 +0800

    add operators

---
+++ blocks/operators.js
@@ -0,0 +1,14 @@
+'use strict';
+
+goog.provide('Blockly.CKB.operators');
+
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "ckb_operators_block_number",
+    "message0": "",
+    "output": "Number",
+    "colour": 195,
+    "tooltip": "blockNumber",
+    "helpUrl": ""
+  }
+]);
